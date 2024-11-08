f224ad8ef25ee14649b8e985b446b2cdc2a63f2e
Hariharan2k24 <hariharan956696@gmail.com>
2024-11-08 11:22:11 -0800

    display the html text while server is running on

--- server1.js
+++ server1.js
@@ -3,8 +3,17 @@ const PORT = 3000;
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    res.write('Hi Everyone');
-    res.end();
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile('index.html', (error, data) => {
+        if(error) {
+            res.writeHead(404);
+            res.write('Page not found');
+        }
+        else {
+            res.write(data);
+        }
+        res.end();
+    })
 });
 
 server.listen(PORT, (error) => {
